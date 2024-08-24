6713dfbcc932f4b1b9d7e0d61192ad334c37d3b6
Ahmad Rafi Wirana <ahmad.rafi02@ui.ac.id>
2024-08-25 00:23:14 +0700

    feat: add dark mode

--- app/page.tsx
+++ app/page.tsx
@@ -1,8 +1,12 @@
 import PomodoroTimer from '@/components/PomodoroTimer'
+import DarkModeToggle from '@/components/ui/DarkModeToggle';
 
 export default function Home() {
   return (
-    <main className="flex min-h-screen flex-col items-center justify-center p-24">
+    <main className="flex min-h-screen flex-col items-center justify-center p-12 md:p-24">
+      <header className="p-3 absolute top-0 right-0">
+        <DarkModeToggle />
+      </header>
       <h1 className="mb-8 text-4xl font-bold">Pomodoro Timer</h1>
       <PomodoroTimer />
     </main>
